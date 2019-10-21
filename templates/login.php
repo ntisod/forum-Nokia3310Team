7c89e8e47735d231751b820370aca9508ad6604f
Nokia3310Team <icake.cc@gmail.com>
2019-10-21 12:54:38 +0200

    19/10/21

--- templates/login.php
+++ templates/login.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Info</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="../test/blank.css">
 </head>
 <body>
 
@@ -15,6 +16,8 @@
         // define variables and set to empty values
         $firstnameErr = $lastnameErr = $emailErr = $passwordErr = $passwordcheckErr = "";
         $firstname = $lastname = $email = $password = $passwordcheck = "";
+        $err = false;
+        $target = "";
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -25,34 +28,52 @@
                 // check if name only contains letters and whitespace
                  if (!preg_match("/^[a-zA-Z ]*$/",$firstname)) {
                     $firstnameErr = "Använd bara bokstäver och mellanslag";
+                    $err = true;
                  }
             }
             if (empty($_POST["lastname"])) {
                 $lastnameErr = "Efternamn är obligatoriskt";
+                $err = true;
             } else {
                 $lastname = test_input($_POST["lastname"]);
             }
 
             if (empty($_POST["email"])) {
                 $emailErr = "E-postadress är obligatoriskt";
+                $err = true;
             } else {
                 $email = test_input($_POST["email"]);
             }
 
             if (empty($_POST["password"])) {
                 $passwordErr = "lösenord är obligatoriskt";
+                $err = true;
             } else {
                 $password = test_input($_POST["password"]);
             }
 
             if (empty($_POST["passwordcheck"])) {
                 $passwordcheckErr = "lösenord är obligatoriskt";
-            } 
-            else if ( $password !== $passwordcheck || $passwordcheck !== $password ) {
-                $passwordcheckErr = "Skriv korrekt lösenord";
+                $err = true;
             }
             else {
                 $passwordcheck = test_input($_POST["passwordcheck"]);
+            }
+
+            if ($_POST["password"] == $_POST["passwordcheck"])  {
+                echo $passwordcheck . "<br>";
+            }
+            else{
+                $passwordcheckErr = "lösenord är fel";
+                $passwordErr = "lösenord är fel";
+                $err = true;
+            }
+
+            if ($err == false){
+                $target = "welcome.php";
+            }
+            else{
+                $target = $_SERVER["PHP_SELF"];
             }
 
             echo $firstname . "<br>";
@@ -71,30 +92,30 @@
         }
     ?>    
 
-    <form action="<?php $_SERVER["PHP_SELF"];?>" method="post">
+    <form  class="w3-container" action="<?php $target;?>" method="post">
         <fieldset>
             <legend>Personliga uppgifter</legend>
             <label for="firstname">Förnamn:</label><br>
-            <input type="text" name="firstname" value="<?php echo $firstname;?>"><span class="error">*
+            <input class="w3-input" type="text" name="firstname" value="<?php echo $firstname;?>"><span class="error">*
             <?php echo $firstnameErr;?></span><br><br>
 
             <label for="lastname">Efternamn:</label><br>
-            <input type="text" name="lastname" value="<?php echo $lastname;?>"><span class="error">*
+            <input class="w3-input" type="text" name="lastname" value="<?php echo $lastname;?>"><span class="error">*
             <?php echo $lastnameErr;?></span><br><br>
 
             <label for="email">E-post:</label><br>
-            <input type="email" name="email" value="<?php echo $email;?>"><span class="error">*
+            <input class="w3-input" type="email" name="email" value="<?php echo $email;?>"><span class="error">*
             <?php echo $emailErr;?></span><br><br>  
 
             <label for="password">Lösenord:</label><br>
-            <input type="text" name="password" value="<?php echo $password;?>"><span class="error">*
-            <?php echo $password;?></span><br><br>  
+            <input class="w3-input" type="text" name="password" value="<?php echo $password;?>"><span class="error">*
+            <?php echo $passwordErr;?></span><br><br>  
 
-            <label for="passwordcheck">Repetera lösenord:</label><br>
-            <input type="text" name="passwordcheck" value="<?php echo $passwordcheck;?>"><span class="error">*
-            <?php echo $passwordcheck;?></span><br><br>  
+            <label for="passwordcheck">Bekräfta lösenord:</label><br>
+            <input class="w3-input" type="text" name="passwordcheck" value="<?php echo $passwordcheck;?>"><span class="error">*
+            <?php echo $passwordcheckErr;?></span><br><br>  
 
-            <input type="submit" value="Registrera">
+            <input class="w3-input" type="submit" value="Registrera">
         </fieldset>
     </form>
 
---
+++ templates/welcome.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="../test/blank.css">
+    <title>Welcome</title>
+</head>
+<body>
+<?php require '../templates/header.php'; ?>
+
+<?php
+
+
+
+
+function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        ?>
+
+<?php require '../templates/footer.php'; ?>
+</body>
+</html>
