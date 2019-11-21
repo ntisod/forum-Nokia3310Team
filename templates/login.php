649c8373629d55cd8856987c41ffc83cea842114
Nokia3310Team <icake.cc@gmail.com>
2019-11-21 14:36:19 +0100

    19/11/21

--- templates/login.php
+++ templates/login.php
@@ -3,40 +3,38 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sv">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Logga in</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../test/blank.css">
+    <title>Inloggning</title>
 </head>
 <body>
 
-
 <?php
-    //test sida
-    if ($_SESSION["REQUEST_METHOD"] == "POST"){
-        //ange session
+    //OBS! Väldigt osäker sida. Inga kontroller här. De till att ha sådana i dina riktiga sidor.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //Ange sessionsvariabel
         $_SESSION["user"]=$_POST["user"];
-
+        //Skicka till session.php
         header("Location: session.php");
-
     }else{
-
+        //Visa inloggningsformulär
         echo <<<HTML
-            <form action="{&_SERVER['PHP_SELF']}" metdon="post">
-            <label for="user">Ange ditt namn: </label>
-            <input type="text" name="user">
-            <input type="submit" value="logga in">
-
-
+            <form action="{$_SERVER['PHP_SELF']}" method="post">
+                <label for="user">Ange ditt användarnamn:</label>
+                <input type="text" name="user">
+                <input type="submit" value="Logga in">
+            </form>
 HTML;
     }
 ?>
 
-<form action="session.php"></form>
-    <label for="user">Ange ditt användarenamn: </label>
-    <input type="text" name="user">
-    <input type="submit" value="logga in">
 </body>
 </html>
--- templates/session.php
+++ templates/session.php
@@ -3,27 +3,33 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sv">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Session</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../test/blank.css">
+    <title>huvudsida</title>
 </head>
 <body>
-    <?php
-    //kolla om du är inloggad
-    if(isset(&_SESSION['user'])){
-        //Om inloggad 
-        echo "<h1>Du är inloggad</h1>";
-        //Möjlighet att logga ut om inloggad
-        echo "<p><a href='logout.php'</a></p>";
-        
+
+<?php
+    //Kolla om du är inloggad
+    if(isset($_SESSION['user'])){
+        //Om inloggad
+        echo "<h1>Du är inloggad som {$_SESSION['user']} </h1>";
+        //Möjlighet att logga ut
+        echo "<p><a href='logout.php'>Logga ut</a></p>";
     }else{
         //Om inte inloggad
         echo "<h1>Du är inte inloggad</h1>";
-        // Möjlighetet att logga in eller registrera sig
-        echo "<p><a href='login.php'</a></p>";
-    ?>
+        //Möjlighet att logga in eller registrera sig
+        echo "<p><a href='login.php'>Logga in</a></p>";
+    }
+?>
 </body>
 </html>
